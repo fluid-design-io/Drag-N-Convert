8045f7cfa361853576d847cf143372c50cee9679
Oliver Pan <2216991777@qq.com>
2025-02-16 11:59:06 -0600

    Initial Commit

---
+++ Drag-N-Convert/Drag_N_ConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drag_N_ConvertApp.swift
+//  Drag-N-Convert
+//
+//  Created by Oliver Pan on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Drag_N_ConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
